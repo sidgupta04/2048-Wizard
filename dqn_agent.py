d01e7a82af064abfdc0ac99209a4ffbcceb1a1f1
rachutha <rachutha@uci.edu>
2026-02-06 15:06:42 -0800

    Updated dqn agent and deletedsmartitect

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,5 +1,17 @@
 import random
+import torch
+import torch.nn
 from collections import deque
+
+
+# Will need to create a QNetwork class
+class QNetwork(nn.Module):
+    def __init__(self, state_size, action_size):
+        super.__init__()
+
+
+        self
+
 
 class DQNAgent:
     def __init__(self):
@@ -10,6 +22,9 @@ class DQNAgent:
         self.epsilon = 1.0
         self.epsilon_min = 0.05
         self.epsilon_decay = 0.995
+        self.q_network = QNetwork(self.state_size, self.action_size)
+        self.q_network.eval()
+
 
     def store(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
